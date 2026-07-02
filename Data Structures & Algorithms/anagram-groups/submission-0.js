522196eab4ac082be93ee507b2e19e2581300dee
Rehana Abdulber <32738176+rehash-it@users.noreply.github.com>
2026-07-02 19:52:38 +0300

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let groupAnagram = {}
+        let alphabetHash = new Map();
+
+        
+        for(let i=0; i <strs.length;i++){
+            let alphabetArray = new Array(26).fill(0);  
+            for ( let j=0; j< strs[i].length ;j++){
+                alphabetArray[strs[i].charCodeAt(j)-97]++
+            }
+           const stringKey= JSON.stringify(alphabetArray);
+
+            if(!groupAnagram[stringKey]){
+                groupAnagram[stringKey] = []
+
+            }
+            groupAnagram[stringKey].push(strs[i])
+            
+        }
+        const result = Object.values(groupAnagram);
+        return result
+        
+    }
+}
